0d7e49c6f6d23e5ee73021053f99596957bb776b
arichilles <arifnd77@gmail.com>
2021-04-19 21:19:29 +0800

    model3 form login

---
+++ model3/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from models import User
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['GET','POST'])
+def index():
+    model = User()
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        model.setUsername(username)
+        model.setPassword(password)
+        
+        if model.authentication():
+            return render_template('login_sukses.html', model=model)
+        else:
+            return render_template('login_error.html')
+    else:
+        return render_template('login_form.html', model=model)
+    
+if __name__ == "__main__":
+    app.run(debug=True)
