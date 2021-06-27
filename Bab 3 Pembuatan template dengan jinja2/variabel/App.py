6365aeec1d7588ff0ab2b88425663a09c5dae130
arichilles <arifnd77@gmail.com>
2021-06-28 06:16:22 +0800

    bab 3 materi ke 1- variabel

---
+++ Bab 3 Pembuatan template dengan jinja2/variabel/App.py
@@ -0,0 +1,27 @@
+from logging import debug
+from flask import Flask, render_template
+from models import Lingkaran
+
+
+application = Flask(__name__)
+
+@application.route('/')
+def Index():
+    str_var = 'Python dan Flask'
+    int_var = 13
+    float_var = 234.456
+    list_var = [10,20,30]
+    dict_var = {'satu': 1, 'dua': 2, 'tiga': 3}
+    model = Lingkaran(5.0)
+    
+    # mengirim nila ke template
+    return render_template('index.html',
+                           str_var=str_var,
+                           int_var=int_var,
+                           float_var=float_var,
+                           list_var=list_var,
+                           dict_var=dict_var,
+                           model=model)
+    
+if __name__ == "__main__":
+    application.run(debug=True)
